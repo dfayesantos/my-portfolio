3ef39d4dba78ddac797c8dbb63abebd5ca23a78c
dfayesantos <109103588+dfayesantos@users.noreply.github.com>
2025-09-18 02:01:43 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   /* config options here */
+      reactStrictMode: true,
 };
 
 export default nextConfig;
