d1f80ed6e4fc8d4a1ac9d2b549a2c4b0a1739ad9
dfayesantos <109103588+dfayesantos@users.noreply.github.com>
2025-09-18 13:06:23 -0400

    deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone' // ensures proper Vercel deployment
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+export default nextConfig
