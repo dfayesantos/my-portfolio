32c67a2a11101ae2ec145556ca8152ed04d27415
dfayesantos <109103588+dfayesantos@users.noreply.github.com>
2025-11-30 01:57:58 -0500

    Updated Home page formatting

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -8,12 +8,15 @@ export default function Home() {
       <div className="max-w-4xl mx-auto px-4 w-full grid grid-cols-1 md:grid-cols-2 items-center gap-8">
         {/* Text column */}
         <div className="text-center md:text-left order-1 md:order-none">
-          <h1 className="text-6xl md:text-8xl font-extrabold tracking-tight text-slate-900">DIANNE<br/>SANTOS</h1>
-          <h2>Chicago Based Software Engineer</h2>
+          <h1 className="text-6xl md:text-8xl tracking-tight text-slate-900">DIANNE<br/>SANTOS</h1>
+            <h2 className="mt-4 flex items-center justify-center md:justify-start text-lg md:text-2xl font-semibold text-slate-700">
+              <span className="flex-none text-5xl md:text-6xl leading-none mr-3 md:mr-4" style={{ color: '#D3A877' }}>*</span>
+              <span className="leading-tight">Chicago Based Software Engineer</span>
+            </h2>
         </div>
         {/* Text column */}
         <div className="flex items-center justify-center order-2 md:order-none">
-          <Image src={chicagoBackdropHome} alt="Chicago Bean Backdrop" width={400} height={300} className="object-contain" />
+          <Image src={chicagoBackdropHome} alt="Chicago Bean Backdrop" width={800} height={900} className="object-contain" />
         </div>
       </div>
     </section>
